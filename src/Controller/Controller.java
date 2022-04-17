3bc40716d6ce6cfd206a0dcf0051815dca097d14
Royc30ne <79168138+Royc30ne@users.noreply.github.com>
2022-04-17 21:11:32 +0100

    Update: STORE COMMAND

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -4,19 +4,48 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Controller {
+    private enum FileState{
+        STORE_IN_PROGRESS,
+        STORE_COMPLETE
+    }
+    public class Client {
+        Integer port;
+        Socket socket;
+        PrintWriter clientWrite;
+
+        public Client(Integer port, Socket socket) {
+            this.port = port;
+            this.socket = socket;
+        }
+        
+        public void sendClientMsg(String msg) {
+            try {
+                PrintWriter clientWrite = new PrintWriter(socket.getOutputStream(), true);
+                clientWrite.println(msg);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+    private Client currentClient = null;
     private final int cport;
     private final int r;
     private final int timeout;
     private final int rebalanced_period; 
     private AtomicInteger countDStore = new AtomicInteger(0); // count number of connected dstores
     private AtomicBoolean dStoreReady = new AtomicBoolean(false);
+    private ConcurrentHashMap<String, FileState> fileIndex = new ConcurrentHashMap<>(); //Index of file with state
     private ConcurrentHashMap<Integer, Socket> dStoreConnections = new ConcurrentHashMap<>(); //Bind client socket with dstore port
+    private ConcurrentHashMap<String, Integer> ackReceive = new ConcurrentHashMap<>(); //Count received acks for each file
 
     public Controller(int cport, int r, int timeout, int rebalanced_period) {
         this.cport = cport;
@@ -30,24 +59,26 @@ public class Controller {
             System.out.println("DStore server starting...");
             ServerSocket serverSocket = new ServerSocket(cport);
             System.out.println("Waiting for Connection");
-
+            
             while(true) {
                 Socket client = serverSocket.accept();
                 System.out.print("Client: " + serverSocket.getInetAddress().getLocalHost() + " has connected to DS server. \n");
                 
                 new Thread(() -> {
+                    boolean isDStore = false;
+                    Integer currentDStorePort = null;
                     try {
                         BufferedReader clinetRead = new BufferedReader(new InputStreamReader(client.getInputStream()));
                         PrintWriter clientWrite = new PrintWriter(client.getOutputStream(), true);
-                        boolean isDStore = false;
                         String clientInput = null;
-                        Integer currentDStorePort = null;
+                        // client.setSoTimeout(timeout);
 
                         while(true) {
                             clientInput = clinetRead.readLine();
-                            System.out.println(clientInput);
 
-                            if (clientInput != null && !dStoreReady.get()){
+                            if (clientInput != null) {
+                                System.out.println(clientInput);
+
                                 String[] commands = clientInput.split(" ");
 
                                 //Recognize DStore
@@ -73,12 +104,15 @@ public class Controller {
                                         dStoreReady.set(true);
                                     }
                                     clientWrite.println(Protocal.JOIN_SUCCESS_TOKEN);
-                                    
                                     continue;
+
+                                } else {
+                                    //Set client connection state
+                                    currentClient = new Client(client.getPort(), client);
                                 }
 
                                 //Operations with Client (DStore are totally connected)
-                                if(!isDStore && dStoreReady.get()) {
+                                if (!isDStore && dStoreReady.get()) {
 
                                     //----------Client----------
                                     //COMMAND: LIST
@@ -86,32 +120,125 @@ public class Controller {
                                         clientWrite.println(clientList());
                                     }
 
+                                    //COMMAND: STORE
+                                    if (commands[0].equals(Protocal.STORE_TOKEN)) {
+                                        //Check length of STORE command from Client
+                                        if (commands.length != 3) {
+                                            //TODO: Add Log
+                                            System.err.println("Wrong STORE command");
+                                            continue;
+                                        } 
+                                        //If DStores are enough
+                                        else if (!dStoreReady.get()) {
+                                            clientWrite.println(Protocal.ERROR_NOT_ENOUGH_DSTORES_TOKEN);
+                                            continue;
+                                        }
+                                        
+                                        String fileName = commands[1];
+                                        Integer fileSize = Integer.parseInt(commands[2]);
+
+                                        //If fileName duplicates
+                                        if (fileIndex.containsKey(fileName)) {
+                                            clientWrite.println(Protocal.ERROR_FILE_ALREADY_EXISTS_TOKEN);
+                                            continue;
+                                        }
+                                        
+                                        fileIndex.put(fileName, FileState.STORE_IN_PROGRESS);
+                                        ackReceive.put(fileName, 0);
+                                        clientWrite.println(Protocal.STORE_TO_TOKEN + listExistPort());
+                                        boolean storeComplete = false;
+                                        
+                                        //Timeout Setting
+                                        while(System.currentTimeMillis() <= System.currentTimeMillis() + timeout) {
+                                            if(ackReceive.get(fileName) >= r) {
+                                                clientWrite.println(Protocal.STORE_COMPLETE_TOKEN);
+                                                fileIndex.remove(fileName);
+                                                fileIndex.put(fileName, FileState.STORE_COMPLETE);
+                                                storeComplete = true;
+                                                break;
+                                            }
+                                        }
+
+                                        if (!storeComplete) {
+                                            System.out.println(fileName + " Store timeout");
+                                        }
+                    
+                                        ackReceive.remove(fileName);
+                                    }
+                                    
+                                    
                                 } 
                                 
                                 //Operations with Client (DStore aren't totally connected)
                                 else if(!isDStore && !dStoreReady.get()) {
-                                        
+                                    clientWrite.println(Protocal.ERROR_NOT_ENOUGH_DSTORES_TOKEN);
                                 }
 
                                 //Operations with DStore
                                 else {
-                                    
+                                    //COMMAND: STORE_ACK filename
+                                    if(commands[0].equals(Protocal.STORE_ACK_TOKEN)) {
+                                        if(commands.length != 2) {
+                                            System.err.println("Wrong STORE_ACK command format");
+                                        }
+                                        String fileName = commands[1].trim();
+
+                                        //Update File Index State when receive ack
+                                        if(ackReceive.containsKey(fileName)) {
+                                            Integer currentValue = ackReceive.get(fileName);
+                                            ackReceive.remove(fileName);
+                                            ackReceive.put(fileName, currentValue + 1);
+                                        } else {
+                                            System.err.println("ACK file not exists");
+                                        }
+                                    }
 
                                 }
+                            } else {
+                                if (isDStore) {
+                                    System.out.println("ERROR: DStore Disconnected!");
+                                    dStoreConnections.remove(currentDStorePort);
+                                    countDStore.decrementAndGet();
+                                    dStoreReady.set(false);
+                                    System.out.println("Current connected DStore: " + countDStore.get() + "/" + this.r);
+                                } else {
+                                    System.out.println("ERROR: Client Disconnected!");
+                                }
+                                client.close();
+                                break;
                             }
-                        }
+                        }   
 
-                    } catch (IOException e) {
-                        // TODO Auto-generated catch block
+                    } catch (Exception e) {
+                        if (isDStore) {
+                            System.out.println("ERROR: DStore Disconnected!");
+                            dStoreConnections.remove(currentDStorePort);
+                            countDStore.decrementAndGet();
+                            dStoreReady.set(false);
+                        }
                         e.printStackTrace();
+                        System.out.println("Current connected DStore: " + countDStore.get() + "/" + this.r);
+                        return;
                     }
                 }).start();
             }
 
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void checkDStoreEnough() {
+
+    }
+
+    private String listExistPort() {
+        String list = "";
+        for (Integer port : dStoreConnections.keySet())
+        {
+            list += Integer.toString(port) + " ";
+        }
+        return list;
     }
 
     private String clientList() {
--- src/DStore/DStore.java
+++ src/DStore/DStore.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class DStore {
@@ -11,6 +14,8 @@ public class DStore {
     private final int cport;
     private final int timeout;
     private String file_folder;
+    private String filePath;
+    private boolean controllerConnected = false;
 
     public DStore(int port, int cport, int timeout, String file_folder) {
         this.port = port;
@@ -29,7 +34,7 @@ public class DStore {
         }
         
         //Initialise DStore folder
-        final String store_path = folder.getAbsolutePath();
+        filePath = folder.getAbsolutePath();
         clearFolder(folder);
 
         try {
@@ -37,48 +42,27 @@ public class DStore {
             Socket controller = new Socket(InetAddress.getByName("localhost"), cport);
             System.out.println("Controller Connected.\nPort: " + cport);
             
-            new Thread(() -> {
-                //Controller Operations
-                try {
-                    BufferedReader readController = new BufferedReader(new InputStreamReader(controller.getInputStream()));
-                    PrintWriter sendController = new PrintWriter(controller.getOutputStream(), true);
-                
-                    String readline = null;
-                    
-                    //Join Controller
-                    sendController.println(Protocal.JOIN_TOKEN + " " + port);
+            new ControllerThread(controller).start();
 
-                    //Process Operations
-                    while(true) {
-                        try {
-                            readline = readController.readLine().trim();
-                            
-                            if(readline != null) {
-                                String[] splits = readline.split(" ");
-                                var command = splits[0].trim();
-                                
-                                if(command.equals(Protocal.JOIN_SUCCESS_TOKEN)) {
-                                    System.out.println("Successfully build connection with Controller");
-                                }
-
-                            }
-                        } catch (IOException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                    } 
-                } catch (IOException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
+            if (!controllerConnected) {
+                return;
+            } 
+            
+            try {
+                ServerSocket serverSocket = new ServerSocket(port);
+                while(true) {
+                    Socket client = serverSocket.accept();
+                    new ClientThread(client, controller).start();
                 }
-            }).start();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-
     }
 
     public void clearFolder(File folder) {
@@ -90,6 +74,130 @@ public class DStore {
         } catch (Exception e) {
             System.err.println(e);
         }
+    }
+
+    /**
+     * Thread to process Controller
+     */
+    class ControllerThread implements Runnable {
+        private Thread controllerThread;
+        private Socket controller;
+
+        public ControllerThread(Socket controller) {
+            this.controller = controller;
+        }
+
+        @Override
+        public void run() {
+            //Controller Operations
+            try {
+                BufferedReader readMsg = new BufferedReader(new InputStreamReader(controller.getInputStream()));
+                PrintWriter sendMsg = new PrintWriter(controller.getOutputStream(), true);
+            
+                String readline = null;
+                
+                //Join Controller
+                sendMsg.println(Protocal.JOIN_TOKEN + " " + port);
+
+                //Process Operations
+                while(true) {
+                        readline = readMsg.readLine().trim();
+                        if(readline != null) {
+                            String[] splits = readline.split(" ");
+                            var command = splits[0].trim();
+
+                            //COMMAND: JOIN
+                            if(command.equals(Protocal.JOIN_SUCCESS_TOKEN)) {
+                                controllerConnected = true;
+                                System.out.println("Successfully build connection with Controller");
+                            }
+                        }
+
+                } 
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                controllerConnected = false;
+            }
+        }
+
+        public void start() {
+            System.out.println("Controller Thread Start");
+            controllerThread = new Thread(this, "Controller Thread");
+            controllerThread.start();
+        }
+
+    }
+
+    /**
+     * Thread to process Client
+     */
+    class ClientThread implements Runnable {
+        private Thread clientThread;
+        private Socket client;
+        private Socket controller;
+        public ClientThread(Socket client, Socket controller) {
+            this.client = client;
+            this.controller =controller;
+        }
+
+        @Override
+        public void run() {
+            try {
+                PrintWriter sendController = new PrintWriter(controller.getOutputStream(), true);
+                PrintWriter sendClinet = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader receiveClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                InputStream writeStream = client.getInputStream();
+                String commandLine;
+                System.out.println("Client connected");
+
+                while(true) {
+                    commandLine = receiveClient.readLine();
+                    if(commandLine != null) {
+                        String[] commands = commandLine.split(" ");
+                        String command = commands[0].trim();
+                        System.out.println("Client Command: " + command);
+                        
+                        //COMMAND: STORE
+                        if(command.equals(Protocal.STORE_TOKEN)) {
+                            //Receive STORE command from Client
+                            if(commands.length != 3) {
+                                System.err.println("Wrong STORE command");
+                                continue;
+                            } else {
+                                sendClinet.println(Protocal.ACK_TOKEN);
+                            }
+
+                            //Receive file stream from Client
+                            String fileName = commands[1].trim();
+                            Integer fileSize = Integer.parseInt(commands[2]);
+                            File file = new File(filePath + File.separator + fileName);
+                            FileOutputStream fos = new FileOutputStream(file);
+                            
+                            while(System.currentTimeMillis() <= System.currentTimeMillis() + timeout) {
+                                fos.write(writeStream.readNBytes(fileSize));
+                                sendController.println(Protocal.STORE_ACK_TOKEN + " " + fileName);
+                                break;
+                            }
+
+                            fos.flush();
+                            fos.close();
+                            client.close();
+                            return;
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } 
+        }
+
+        public void start() {
+            System.out.println("Client Thread Start.");
+            clientThread = new Thread(this, "Client Thread");
+            clientThread.start();
+        }
+        
     }
 
     public static void main(String[] args) {
